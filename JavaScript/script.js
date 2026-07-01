8ff827c7c23d5233545ab6c4fe2a04116687508a
Emmanuel Johnson <leunammejohnson17@gmail.com>
2026-07-01 21:13:29 +0100

    Variables in JS

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+console.log("Holla Amigos, I'm learning JavaScript.");
+
+// Variables: They are used to store resusable values
+// Definition can be: let, const, and var (not really supported)
+
+let nickName = "Manny";
+console.log(nickName);
